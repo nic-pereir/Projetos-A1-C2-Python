e98c34cda238ec3a07a97c22899d231634ef9166
Nicolly Pereira <nicollycontato12@gmail.com>
2026-01-16 21:22:21 -0300

    ajustes

--- A2 Level/finalA2.py
+++ A2 Level/finalA2.py
@@ -1,7 +1,7 @@
 # Criar um To-do List que contenha funções de: adicionar, remover e mudar status de cada tarefa.
 
 task = list()
-checklist = dict(status = "P" or "C", description = str)
+checklist = dict()
 
 def show_menu():
     print("=" * 30)
@@ -23,13 +23,13 @@ def enter():
 show_menu()
 
 while True:
-    print()
     awnser = int(input("Escolha uma opção: "))
     if awnser == 1:
         print("Digite a descrição da tarefa: ")
         checklist["status"] = "[ ]"
         checklist["description"] = str(input("> "))
-        task.append(checklist)
+        task.append(checklist.copy())
+        checklist.clear()
         print()
         print("Tarefa adicionada com sucesso!")
         enter()
@@ -40,17 +40,33 @@ while True:
         print("     TAREFAS CADASTRADAS:     ")
         print()
         for i, v in enumerate(task):
-            i +=1
-            print(f"{i} - {v}")   
+            i+=1
+            print(f"{i} - {v['status']}  {v['description']}")
         enter()
+
     elif awnser == 3:
         print()
         print("Digite o número da tarefa concluída: ")
         status = int(input("> "))
-        if status not in task:
-            print("Tente novamente.")
-            show_menu()
-        else:
-
+        indic = status - 1
+        if 0 <= indic < len(task):
+            task[indic]["status"] = "[x]"
             print("Tarefa marcada como concluída!")
+        else:
+            print("Número inválido. Essa tarefa não existe.")
         enter()
+
+    elif awnser == 4:
+        print()
+        print("Digite o número da tarefa a ser removida:")
+        remove = int(input("> "))
+        index = remove - 1
+        if 0 <= index < len(task):
+            del task[index]
+            print("Tarefa removida com sucesso.")
+        else:
+            print("Número inválido. Essa tarefa não existe.")
+        enter()
+    
+    else:
+        print("Saindo...")
